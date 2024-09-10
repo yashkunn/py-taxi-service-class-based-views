ce8e26797f666118637909d799d3ad2126f768a3
Yakiv Lobach <lobachyakiv@gmail.com>
2024-09-10 19:04:48 +0300

    second_try

--- taxi/urls.py
+++ taxi/urls.py
@@ -11,8 +11,11 @@ from .views import (
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/", ManufacturerListView.as_view(),
-         name="manufacturer-list"),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list"
+    ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
